0894270eeb061f4db99f2c0cb79695a05ece65e5
Nvieira0511 <c-nathan-c@hotmail.com>
2023-04-26 14:44:34 -0400

    express integration

---
+++ Server.js
@@ -0,0 +1,14 @@
+const { log } = require("console");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+  
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
